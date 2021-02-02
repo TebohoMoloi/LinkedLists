3609ebd045c7c9f0b020b35b92b83d6bf21bf02c
TebohoMoloi <52884661+TebohoMoloi@users.noreply.github.com>
2021-02-02 17:09:37 +0200

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,16 +5,17 @@ import java.util.LinkedList;
 public class Main {
 
     public static void main(String[] args) {
-	    LinkedList<String> names = new LinkedList<>();
-	    names.add("Teboho");
-	    names.add("Moloi");
-	    names.add("Sylvester");
-		for (String name : names) {
-			if (name.equals("Teboho"))
-				System.out.println("We found it bro!");
-			else
-				System.out.println("Shit man, i ain't found nothing");
-			break;
-		}
+ 	LinkedList<String> names = new LinkedList<>();
+        names.add("Teboho");
+        names.add("Moloi");
+        names.add("Sylvester");
+        String whatever = "Shit man, i ain't found nothing";
+        for (String name : names) {
+            if (name.equals("Teboho")) {
+                whatever = "We found it bro!";
+                break;
+            }
+        }
+        System.out.println(whatever);
     }
 }
